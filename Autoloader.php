a35db3685a36b995a746386003f4c6a2b87d160d
Jorge <Jorge>
2012-08-05 21:21:20 -0700

    autoloader dir

--- Autoloader.php
+++ Autoloader.php
@@ -2,7 +2,7 @@
 
 spl_autoload_register(function($class){
   $class = str_replace('Freeform\\', '', $class);
-  $file = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+  $file = __DIR__ . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
   if (is_readable($file))
   {
     include_once $file;
